022151ff66997d2e99377ddb00d85d41c6558d71
Carsten Haubold <carsten.haubold@knime.com>
2022-01-25 16:14:32 +0100

    WIP: Add example script to set up local channels

---
+++ org.knime.python3.conda.recipes/example.sh
@@ -0,0 +1,25 @@
+# create environments for base and mdf, searching for the knime-python metapackages in conda-bld
+micromamba env create -n basedepsmamba knime-python-base -c /home/chaubold/miniconda3/conda-bld -c conda-forge --override-channels
+micromamba env create -n mdfdepsmamba knime-python-mdf -c /home/chaubold/miniconda3/conda-bld -c conda-forge --override-channels
+
+# get list of all required packages in those environments
+micromamba activate basedepsmamba
+conda list --explicit > basedepsmamba.txt # must be done with conda, micromamba doesn't have the "explicit" flag
+micromamba activate mdfdepsmamba
+conda list --explicit > mdfdepsmamba.txt
+
+# Download all required packages to our own local "channel".
+# A "linux-64" subfolder is needed because conda sorts packages by architecture like this
+# (needs python and requests in the active env)
+python /home/chaubold/src/knime-python/org.knime.python3.conda.recipes/get_packages.py basedepsmamba.txt --dest base_channel/linux-64
+python /home/chaubold/src/knime-python/org.knime.python3.conda.recipes/get_packages.py mdfdepsmamba.txt --base basedepsmamba.txt --dest mdf_channel/linux-64
+# create package repo index
+conda index base_channel
+conda index mdf_channel
+
+# create envs from the local channels
+micromamba create -n basechanneltest -c ./base_channel --override-channels knime-python-base
+micromamba create -n mdfchanneltest -c ./base_channel -c ./mdf_channel --override-channels knime-python-mdf
+# these also work
+# conda create -n basechanneltest -c ./base_channel --override-channels knime-python-base
+# conda create -n mdfchanneltest -c ./base_channel -c ./mdf_channel --override-channels knime-python-mdf
