60e2079b644d7ad7119f448f230d608c2c05c4c2
Adrian Nembach <adrian.nembach@knime.com>
2022-04-25 14:44:44 +0200

    AP-18799: Verification node
    Only for verification purposes, should not be merged to master.
    
    AP-18799 (Use bundled conda environment for Python node extensions)

---
+++ org.knime.python3.nodes.testing/src/main/python/my_extension.py
@@ -0,0 +1,27 @@
+from typing import List, Tuple
+import knime_node as kn
+import knime_table as kt
+import knime_schema as ks
+
+
+@kn.node(name="My Node", node_type="Learner", icon_path="icon.png", category="/")
+class MyNode(kn.PythonNode):
+    def __init__(self) -> None:
+        super().__init__()
+        self._some_param = 42
+
+    @kn.Parameter
+    def some_param(self):
+        return self._some_param
+
+    @some_param.setter
+    def some_param(self, value):
+        self._some_param = value
+
+    def configure(self, input_schemas: List[ks.Schema]) -> List[ks.Schema]:
+        import sys
+        print(sys.executable)
+        return input_schemas
+
+    def execute(self, tables: List[kt.ReadTable], objects: List, exec_context) -> Tuple[List[kt.WriteTable], List]:
+        return [kt.write_table(table) for table in tables], []
